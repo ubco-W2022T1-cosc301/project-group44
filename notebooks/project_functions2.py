bc25332d6143589c162661a18ab21d6ddd2a22a8
Isabella Mendoza <igmendoza2003@gmail.com>
2022-11-03 23:09:12 -0700

    Isabella - finished task 3, starting task 4

---
+++ notebooks/project_functions2.py
@@ -0,0 +1,19 @@
+#Isabella Mendoza's function
+
+import pandas as pd
+
+def load_and_process(rawData):
+    
+    #process data
+    df1 = (
+        pd.read_csv(rawData)
+        .drop(['region','children', 'sex'], axis=1)
+        .dropna(axis=0)
+    )
+    
+    #alter representation of values
+    df1['smoker'] = df1['smoker'].map({'yes': 1, 'no': 0})
+    df1['bmi'] = df1['bmi'].round(decimals = 2)
+    df1['charges'] = df1['charges'].round(decimals = 2)
+    
+    return df1
