9b19bfe75f8fdc77f14526e051b5fcbb4eb64d97
wenverga <matthewenverga@gmail.com>
2022-11-22 22:58:36 -0800

    Changed data

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -6,7 +6,6 @@ def load_and_process(rawData):
     
     df_clean = (pd.read_csv(rawData).drop(['children', 'sex'], axis=1).dropna(axis=0))
     
-    df_clean['smoker'] = df_clean['smoker'].map({'yes': 1, 'no': 0})
     df_clean['bmi'] = df_clean['bmi'].round(decimals = 2)
     df_clean['charges'] = df_clean['charges'].round(decimals = 2)
     
